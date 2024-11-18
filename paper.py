ae36a7780e8129b1030516f6e69264411e7a2f46
VAS <projetos.vas@gmail.com>
2024-11-17 22:05:35 -0300

    Implementação gráfica

--- paper.py
+++ paper.py
@@ -13,17 +13,17 @@ import matplotlib.pyplot as plt
 class Manipulador:
     def __init__(self):
         self.origem = np.array([0, 0, 0]) # Origem do manipulador
-        self.j1_position = np.array([0, 0, 1]) # Posição da primeira Junta
-        self.j2_position = np.array([1, 0, 1]) # Posição da segunda Junta
-        self.j3_position = np.array([2, 0, 1]) # Posição da terceira Junta
+        self.j1_position = np.array([0, 0, 1]) # Posição da primeira Junta em referecia a origem
+        self.j2_position = np.array([1, 0, 0]) # Posição da segunda Junta em referencia a J1
+        self.j3_position = np.array([1, 0, 0]) # Posição da terceira Junta em referencia a J2
 
         self.j1_axis = np.array([0, 0, 1]) # Eixo de rotação da primeira Junta
         self.j2_axis = np.array([0, 1, 0]) # Eixo de rotação da segunda Junta
         self.j3_axis = np.array([0, 1, 0]) # Eixo de rotação da terceira Junta
 
-        self.j1_oldposition = np.array([0, 0, 0]) # Posição anterior da primeira junta
-        self.j2_oldposition = np.array([0, 0, 0]) # Posição anterior da segunda junta
-        self.j3_oldposition = np.array([0, 0, 0]) # Posição anterior da terceira junta
+        self.j1_oldposition = np.array([0, 0, 0]) # Posição anterior da primeira junta em referencia a origem
+        self.j2_oldposition = np.array([0, 0, 0]) # Posição anterior da segunda junta em referencia a J1
+        self.j3_oldposition = np.array([0, 0, 0]) # Posição anterior da terceira junta em referencia a J2
 
 
     def quaternion(self, angle, axis): # Função para gerar um quaternion
@@ -54,34 +54,36 @@ class Manipulador:
         ])
     
 
-    def rot_quaternion(self, body, axis_point, q): # Função que realiza a rotação
+    def rot_quaternion(self, body, q): # Função que realiza a rotação
         # A função recebe o vetor do corpo rígido e quaternion de rotação
 
         q_conjugate = np.array([q[0], -q[1], -q[2], -q[3]]) # Calcula o conjugado
         body_quaternion = np.array([0,
-                                    body[0]-axis_point[0],
-                                    body[1]-axis_point[1],
-                                    body[2]-axis_point[2]])
+                                    body[0],
+                                    body[1],
+                                    body[2]])
 
         body_rotated = self.mul_quaternion(self.mul_quaternion(q, body_quaternion), q_conjugate)
 
-        return body_rotated[1:] + axis_point
+        return body_rotated[1:]
     
 
 robo = Manipulador()
 theta = np.array([np.radians(0),
-                 np.radians(-45),
-                 np.radians(0)
+                 np.radians(-94),
+                 np.radians(-206)
                  ])
 
 robo.j1_oldposition = robo.j1_position
-robo.j1_position = robo.rot_quaternion(robo.j1_position, robo.origem ,robo.quaternion(theta[0], robo.j1_axis))
+robo.j1_position = robo.rot_quaternion(robo.j1_position, robo.quaternion(theta[0], robo.j1_axis))
 
 robo.j2_oldposition = robo.j2_position
-robo.j2_position = robo.rot_quaternion(robo.j2_position, robo.j1_position ,robo.quaternion(theta[1], robo.j2_axis))
+robo.j2_position = robo.rot_quaternion(robo.rot_quaternion(robo.j2_position, robo.quaternion(theta[0], robo.j1_axis)), robo.quaternion(theta[1], robo.j2_axis))
+robo.j2_position = robo.j2_position + robo.j1_position
 
 robo.j3_oldposition = robo.j3_position
-robo.j3_position = robo.rot_quaternion(robo.j3_position, robo.j2_position ,robo.quaternion(theta[2], robo.j3_axis))
+robo.j3_position = robo.rot_quaternion(robo.rot_quaternion(robo.rot_quaternion(robo.j3_position, robo.quaternion(theta[0], robo.j1_axis)), robo.quaternion(theta[1], robo.j2_axis)), robo.quaternion(theta[2], robo.j3_axis))
+robo.j3_position = robo.j3_position + robo.j2_position
 
 
 print("Posição Inicial J1: {}".format(robo.j1_oldposition))
@@ -91,24 +93,58 @@ print("Posição Final J2: {}".format(robo.j2_position))
 print("Posição Inicial J3: {}".format(robo.j3_oldposition))
 print("Posição Final J3: {}".format(robo.j3_position))
 
+# Interface de Visualização
 
-# fig = plt.figure() # Cria uma figura
-# ax = fig.add_subplot(111, projection='3d')
+fig = plt.figure() # Cria uma figura
+ax = fig.add_subplot(111, projection='3d')
 
-# ax.set_xlim([-3, 3]) # Definicao da escala do eixo x
-# ax.set_ylim([-3, 3]) # Definicao da escala do eixo y
-# ax.set_zlim([-3, 3]) # Definicao da escala do eixo z
 
-# ax.set_xlabel("X") # Titulo do eixo x
-# ax.set_ylabel("Y") # Titulo do eixo y
-# ax.set_zlabel("Z") # Titulo do eixo z
+ax.set_xlim([-2.5, 2.5]) # Definicao da escala do eixo x
+ax.set_ylim([-2.5, 2.5]) # Definicao da escala do eixo y
+ax.set_zlim([0, 2.5]) # Definicao da escala do eixo z
 
-# plt.title("3D Space") # Titulo da figura
+ax.set_xlabel("X") # Titulo do eixo x
+ax.set_ylabel("Y") # Titulo do eixo y
+ax.set_zlabel("Z") # Titulo do eixo z
 
-# # Plotar a posicao inicial do corpo em vermelho
-# ax.quiver(0, 0, 0, robo.j1_oldposition[0], robo.j1_oldposition[1], robo.j1_oldposition[2], color="r") 
+plt.title("Posicionamento do Manipulador") # Titulo da figura
 
-# # Plotar a posicao final do corpo em azul
-# ax.quiver(0, 0, 0, robo.j1_position[0], robo.j1_position[1], robo.j1_position[2], color="b")
+# Plotar a posicao inicial do corpo em vermelho
+ax.quiver(0, 0, 0, robo.j1_oldposition[0], robo.j1_oldposition[1], robo.j1_oldposition[2], color="r") 
 
-# plt.show() # Mostrar o grafico
+ax.quiver(robo.j1_oldposition[0],
+          robo.j1_oldposition[1],
+          robo.j1_oldposition[2],
+          robo.j2_oldposition[0],
+          robo.j2_oldposition[1],
+          robo.j2_oldposition[2],
+          color="r")
+ 
+ax.quiver(robo.j1_oldposition[0] + robo.j2_oldposition[0],
+          robo.j1_oldposition[1] + robo.j2_oldposition[1],
+          robo.j1_oldposition[2] + robo.j2_oldposition[2],
+          robo.j3_oldposition[0],
+          robo.j3_oldposition[1],
+          robo.j3_oldposition[2],
+          color="r") 
+
+# Plotar a posicao final do corpo em azul
+ax.quiver(0, 0, 0, robo.j1_position[0], robo.j1_position[1], robo.j1_position[2], color="b")
+
+ax.quiver(robo.j1_position[0],
+          robo.j1_position[1],
+          robo.j1_position[2],
+          robo.j2_position[0] - robo.j1_position[0],
+          robo.j2_position[1] - robo.j1_position[1],
+          robo.j2_position[2] - robo.j1_position[2],
+          color="b")
+
+ax.quiver(robo.j2_position[0],
+          robo.j2_position[1],
+          robo.j2_position[2],
+          robo.j3_position[0] - robo.j2_position[0],
+          robo.j3_position[1] - robo.j2_position[1],
+          robo.j3_position[2] - robo.j2_position[2],
+          color="b")
+
+plt.show() # Mostrar o grafico
